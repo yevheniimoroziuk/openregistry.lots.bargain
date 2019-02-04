becc411d714b019ec4bbb93dedc62aa44b7de754
Oleksiy Veretiuk <oleksiy.veretiuk@quintagroup.org>
2019-02-04 18:05:08 +0200

    Update Auction model structure

--- openregistry/lots/redemption/models.py
+++ openregistry/lots/redemption/models.py
@@ -1,11 +1,8 @@
 # -*- coding: utf-8 -*-
 from uuid import uuid4
-from datetime import datetime
 from pyramid.security import Allow
-from schematics.types import StringType, IntType, FloatType
-from schematics.exceptions import ValidationError
+from schematics.types import StringType
 from schematics.types.compound import ModelType, ListType
-from schematics.types.serializable import serializable
 from zope.interface import implementer
 from openregistry.lots.core.constants import (
     SANDBOX_MODE,
@@ -20,13 +17,9 @@ from openregistry.lots.core.models import (
     AssetCustodian,
     AssetHolder,
     Model,
-    IsoDateTimeType,
-    IsoDurationType,
     Guarantee,
-    Period,
     Value,
     BankAccount,
-    AuctionParameters,
     RelatedProcess,
 )
 
@@ -37,46 +30,24 @@ from openregistry.lots.core.validation import (
 from openregistry.lots.core.models import (
     ILot,
     Lot as BaseLot,
-    get_lot
 )
-from openregistry.lots.core.utils import (
-    get_now,
-    calculate_business_date,
-    time_dependent_value
-)
-
 from openregistry.lots.redemption.constants import (
     LOT_STATUSES,
     AUCTION_STATUSES,
-    AUCTION_DOCUMENT_TYPES,
     CONTRACT_STATUSES,
     LOT_DOCUMENT_TYPES,
     CURRENCY_CHOICES,
-    DEFAULT_REGISTRATION_FEE_BEFORE_2019,
-    DEFAULT_REGISTRATION_FEE_AFTER_2019
 )
 from openregistry.lots.redemption.roles import (
     lot_roles,
     auction_roles,
     decision_roles,
-    auction_period_roles,
     contracts_roles,
 )
 
 
 class IRedemptionLot(ILot):
     """ Marker interface for basic lots """
-
-
-class StartDateRequiredPeriod(Period):
-    class Options:
-        roles = auction_period_roles
-    startDate = IsoDateTimeType(required=True)
-
-
-class AuctionDocument(Document):
-    documentType = StringType(choices=AUCTION_DOCUMENT_TYPES, required=True)
-    documentOf = StringType(choices=['auction'])
 
 
 class LotDocument(Document):
@@ -89,21 +60,6 @@ class RedemptionValue(Value):
 
 class RedemptionGuarantee(Guarantee):
     currency = StringType(required=True, default=DEFAULT_CURRENCY, choices=CURRENCY_CHOICES, max_length=3, min_length=3)
-
-
-class RegistrationFee(RedemptionGuarantee):
-    amount = FloatType(min_value=0)
-
-    @serializable(serialized_name='amount', serialize_when_none=False, type=FloatType(min_value=0))
-    def serializable_amount(self):
-        if not self.amount:
-            border_date = datetime(year=2019, month=1, day=1, tzinfo=TZ)
-            self.amount = time_dependent_value(
-                border_date,
-                DEFAULT_REGISTRATION_FEE_BEFORE_2019,
-                DEFAULT_REGISTRATION_FEE_AFTER_2019
-            )
-        return self.amount
 
 
 class LotDecision(Decision):
@@ -129,35 +85,13 @@ class Auction(Model):
     auctionID = StringType()
     relatedProcessID = StringType()
     status = StringType(choices=AUCTION_STATUSES)
-    procurementMethodType = StringType(choices=['sellout.english', 'sellout.insider'])
-    tenderAttempts = IntType(min_value=1, max_value=3)
-    auctionPeriod = ModelType(StartDateRequiredPeriod)
+    procurementMethodType = StringType(choices=['procedure.name'])
     value = ModelType(RedemptionValue)
-    minimalStep = ModelType(RedemptionValue)
     guarantee = ModelType(RedemptionGuarantee)
-    registrationFee = ModelType(RegistrationFee, default={})
     bankAccount = ModelType(BankAccount)
-    documents = ListType(ModelType(AuctionDocument), default=list())
-    auctionParameters = ModelType(AuctionParameters)
-    tenderingDuration = IsoDurationType()
-    submissionMethodDetails = StringType()
-    submissionMethodDetails_en = StringType()
-    submissionMethodDetails_ru = StringType()
+
     if SANDBOX_MODE:
         procurementMethodDetails = StringType()
-
-    def validate_minimalStep(self, data, value):
-        if value and value.amount and data.get('value'):
-            if data.get('value').amount < value.amount:
-                raise ValidationError(u"value should be less than value of auction")
-            if data.get('value').currency != value.currency:
-                raise ValidationError(u"currency should be identical to currency of value of auction")
-            if data.get('value').valueAddedTaxIncluded != value.valueAddedTaxIncluded:
-                raise ValidationError(
-                    u"valueAddedTaxIncluded should be identical to valueAddedTaxIncluded of value of auction")
-
-    def validate_auctionPeriod(self, data, period):
-        lot = get_lot(data['__parent__'])
 
     def get_role(self):
         root = self.__parent__.__parent__
@@ -169,7 +103,7 @@ class Auction(Model):
         elif request.authenticated_role == 'concierge':
             role = 'concierge'
         else:
-            role = 'edit_{}.{}'.format(request.context.tenderAttempts, request.context.procurementMethodType)
+            role = 'edit_{}'.format(request.context.procurementMethodType)
         return role
 
 
@@ -209,7 +143,7 @@ class Lot(BaseLot):
     documents = ListType(ModelType(LotDocument), default=list())
     decisions = ListType(ModelType(LotDecision), default=list(), validators=[validate_decision_uniq])
     relatedProcesses = ListType(ModelType(RelatedProcess), default=list(), max_size=1)
-    auctions = ListType(ModelType(Auction), default=list(), max_size=3)
+    auctions = ListType(ModelType(Auction), default=list(), max_size=1)
     contracts = ListType(ModelType(Contract), default=list())
 
     _internal_type = 'redemption'
